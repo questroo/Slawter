d52c308b00ffafeda469ce64cb62634f3264d01b
questroo <42195958+questroo@users.noreply.github.com>
2020-06-26 14:51:22 -0700

    - Initial commit. - Player prefab setup with movement and fps camera. - Folder structure setup.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 10.0f;
+    private CharacterController controller;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 moveDir = transform.right * x + transform.forward * z;
+
+        controller.Move(moveDir * moveSpeed * Time.deltaTime);
+    }
+}
