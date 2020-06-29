9ea37686c145c97febbe53fe977ce6712d89615c
halyosyx <aisle.neral2525@gmail.com>
2020-06-28 21:49:18 -0700

    -Started by adding a new scene where most enemy/AI test will occur - Started on Suicide Bomber enemy - Implemented a basic cube model for the enemy -Added a Enemy Prefab folder in pre fab - Implemented a basic health script with take damage function - Enemy block has nav mesh

---
+++ Assets/Scripts/Enemy Script/EnemyHealth.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float maxHealth = 100.0f;
+
+    private float currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            TakeDamage(15.0f);
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0.0f)
+        {
+            currentHealth = 0.0f;
+            // A: Calls a "Die" state, Destroys Game Object
+            Destroy(gameObject);
+
+        }
+        Debug.Log("Health: " + currentHealth);
+    }
+}
