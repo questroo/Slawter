94df7cdde44be67378ff4f3168dab37dda44038c
questroo <42195958+questroo@users.noreply.github.com>
2020-06-29 15:03:45 -0700

    - Added a jump mechanic to the player in the PlayerMovement.cs.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 10.0f;
+    [SerializeField]
+    private float moveSpeed = 10.0f;
+    [SerializeField]
+    private float jumpForce = 10.0f;
+    [SerializeField]
+    private float gravity = -9.81f;
+
     private CharacterController controller;
+
+    private float verticalDirection;
 
     private void Start()
     {
@@ -16,8 +25,19 @@ public class PlayerMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 moveDir = transform.right * x + transform.forward * z;
+        Vector3 direction = transform.right * x + transform.forward * z;
+        if (controller.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                verticalDirection = jumpForce;
+            }
+        }
 
-        controller.Move(moveDir * moveSpeed * Time.deltaTime);
+        verticalDirection -= gravity * Time.deltaTime;
+
+        direction.y = verticalDirection;
+
+        controller.Move(direction * moveSpeed * Time.deltaTime);
     }
 }
