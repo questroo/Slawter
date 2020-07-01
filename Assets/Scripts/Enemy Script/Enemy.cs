fd7f8b57d3c45b0d950e64fc9038c237059029da
halyosyx <aisle.neral2525@gmail.com>
2020-07-01 15:45:05 -0700

    Updated state machine Added a aptrol state script

---
+++ Assets/Scripts/Enemy Script/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using Assets.Scripts.Enemy_Script.PatrolPoint;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Assets.Scripts.Enemy_Script
+{
+    [RequireComponent(typeof(NavMeshAgent), typeof(FiniteStateMachine))]
+
+    public class Enemy : MonoBehaviour
+    {
+        NavMeshAgent navMeshAgent;
+        FiniteStateMachine finiteStateMachine;
+
+        [SerializeField]
+        EnemyPatrolPoint[] patrolPoints;
+
+        public void Awake()
+        {
+            navMeshAgent = this.GetComponent<NavMeshAgent>();
+            finiteStateMachine = this.GetComponent<FiniteStateMachine>();
+        }
+
+        public void Start()
+        {
+            
+        }
+
+        public void Update()
+        {
+            
+        }
+
+        public EnemyPatrolPoint[] PatrolPoints
+        {
+            get
+            {
+                return patrolPoints;
+            }
+        }
+    }
+}
