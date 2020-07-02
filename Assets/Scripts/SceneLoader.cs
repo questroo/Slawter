f12c50fcfeeaceecaea8c412f79984c9e0500335
questroo <42195958+questroo@users.noreply.github.com>
2020-07-02 15:32:47 -0700

    - Imported TMPro assets. - Added Main menu and options scene. - Added SceneLoader.cs for changing scenes.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadOptions()
+    {
+        SceneManager.LoadScene("Options");
+    }
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    //public void LoadLevelSelect()
+    //{
+    //    
+    //}
+}
