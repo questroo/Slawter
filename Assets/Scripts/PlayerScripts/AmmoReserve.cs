1c0f641bf7c85bb823297605eac78703121a60f3
questroo <42195958+questroo@users.noreply.github.com>
2020-07-10 23:43:03 -0700

    - Added AmmoInventory.cs and AmmoReserve.cs

---
+++ Assets/Scripts/PlayerScripts/AmmoReserve.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AmmoType
+{
+    RifleAmmo,
+    ShotgunAmmo,
+    HandgunAmmo
+}
+
+public class AmmoReserve : MonoBehaviour
+{
+    public int currentRifleAmmo = 120;
+    public int currentShotgunAmmo = 24;
+    public int currentHandgunAmmo = 48;
+}
