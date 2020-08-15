a59257ea30ac396366ed76e3b19e03b31e9cbbab
questroo <42195958+questroo@users.noreply.github.com>
2020-08-15 12:58:29 -0700

    - Added a Spawner.cs and a SpawnerManager.cs. - Created prefab for Spawner and SpawnerManager to spawn enemies.

---
+++ Assets/Scripts/GameScripts/Spawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] spawnableEnemies;
+
+    public void Spawn()
+    {
+        int randomIndex = Random.Range(0, spawnableEnemies.Length - 1);
+        Instantiate(spawnableEnemies[randomIndex], transform);
+    }
+}
---
+++ Assets/Scripts/GameScripts/SpawnerManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerManager : MonoBehaviour
+{
+    private Spawner[] spawners;
+
+    private void Start()
+    {
+        spawners = FindObjectsOfType<Spawner>();
+        InvokeRepeating("SendSpawnCommand", 3.0f, 20.0f);
+    }
+
+    private void SendSpawnCommand()
+    {
+        foreach(Spawner spawner in spawners)
+        {
+            spawner.Spawn();
+        }
+    }
+}
